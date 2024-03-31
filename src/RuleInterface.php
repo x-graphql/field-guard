f36b4aa5377169166208c0353b45294d9d7d93ed
Minh Vuong <vuongxuongminh@gmail.com>
2024-03-31 08:37:30 +0700

    docs: add interface description

--- src/RuleInterface.php
+++ src/RuleInterface.php
@@ -6,9 +6,18 @@ namespace XGraphQL\FieldGuard;
 
 use GraphQL\Type\Definition\ResolveInfo;
 
+/**
+ * Help to manage access control
+ */
 interface RuleInterface
 {
+    /**
+     * Whether to accept user access system.
+     */
     public function allows(mixed $value, array $args, mixed $context, ResolveInfo $info): bool;
 
+    /**
+     * Whether to remember [[allows]] result when accessing field again.
+     */
     public function shouldRemember(mixed $value, array $args, mixed $context, ResolveInfo $info): bool;
 }
